4d7aeee2fde9d71dc7cb73892682910960abb8d8
Raul-71 <rrodmon600@g.educaand.es>
2022-01-13 15:15:03 +0100

    Ejercicio 1

--- Ejercicio1/src/Bicicleta.java
+++ Ejercicio1/src/Bicicleta.java
@@ -1,9 +1,43 @@
 
 public class Bicicleta {
 
-	public static void main(String[] args) {
+	
 		// TODO Esbozo de método generado automáticamente
-
-	}
+		public int velocidadActual;
+		public int platoActual;
+		public int pinonActual;
+		
+		
+		public Bicicleta (int velocidadActual, int platoActual, int pinonActual) {
+			this.velocidadActual = velocidadActual;
+			this.platoActual = platoActual;
+			this.pinonActual = pinonActual;
+		}
+		
+		public void verDatos() {
+			
+			
+		}
+		
+		
+		
+		/*
+		public int getVelocidadActual(){
+			return velocidadActual;
+		}
+		
+		public int getPlatoActual(){
+			return platoActual;
+		}
+		
+		public int getPinonActual() {
+			return pinonActual;
+		}
+		
+		
+		
+		*/
+		
+	
 
 }
